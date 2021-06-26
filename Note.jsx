c4ed4574368981b15960b2a4d32f0cb5871d4d30
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2021-06-26 17:23:52 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>JavaScript and React.js</h1>
+      <p>Basic Web Development Bootcamp</p>
+    </div>
+  );
+}
+
+export default Note;
